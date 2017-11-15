52a63c875741eb957b25d13d05e30aef58f610ee
Kirdyasheva <kirdyasheva98@gmail.com>
2017-11-15 16:22:55 +0300

    hhhhh

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,6 @@
 import javax.sound.midi.*;
 
 public class Program {
-
     public static void main(String[] args) throws MidiUnavailableException, InterruptedException {
         int tonality = 48;
         PSO1 pso = new PSO1(tonality);
@@ -13,45 +12,36 @@ public class Program {
         MidiChannel[] channel2 = synth.getChannels();
         MidiChannel[] channel3 = synth.getChannels();
         for (int i = 0; i < a.length; i++) {
-            playChord(tonality, a[i]);
-        }
-    }
-
-    public static void playChord(int tonality, int note) throws MidiUnavailableException, InterruptedException {
-        Synthesizer synth = MidiSystem.getSynthesizer();
-        synth.open();
-        MidiChannel[] channel1 = synth.getChannels();
-        MidiChannel[] channel2 = synth.getChannels();
-        MidiChannel[] channel3 = synth.getChannels();
-        switch ((note - tonality) % 7) {
-            case 2: {
-                channel1[2].noteOn(note, 60);
-                channel2[2].noteOn(note + 2, 60);
-                channel3[2].noteOn(note + 3, 60);
-                Thread.sleep(500);
-                channel1[2].noteOff(note);
-                channel2[2].noteOff(note + 2);
-                channel3[2].noteOff(note + 3);
-                break;
-            }
-            case 4: {
-                channel1[2].noteOn(note, 60);
-                channel2[2].noteOn(note + 1, 60);
-                channel3[2].noteOn(note + 3, 60);
-                Thread.sleep(500);
-                channel1[2].noteOff(note);
-                channel2[2].noteOff(note + 1);
-                channel3[2].noteOff(note + 3);
-                break;
-            }
-            default: {
-                channel1[2].noteOn(note, 60);
-                channel2[2].noteOn(note + 2, 60);
-                channel3[2].noteOn(note + 4, 60);
-                Thread.sleep(500);
-                channel1[2].noteOff(note);
-                channel2[2].noteOff(note + 2);
-                channel3[2].noteOff(note + 4);
+            switch ((a[i] - tonality) % 7) {
+                case 2: {
+                    channel1[2].noteOn(a[i], 60);
+                    channel2[2].noteOn(a[i] + 2, 60);
+                    channel3[2].noteOn(a[i] + 3, 60);
+                    Thread.sleep(500);
+                    channel1[2].noteOff(a[i]);
+                    channel2[2].noteOff(a[i] + 2);
+                    channel3[2].noteOff(a[i] + 3);
+                    break;
+                }
+                case 4: {
+                    channel1[2].noteOn(a[i], 60);
+                    channel2[2].noteOn(a[i] + 1, 60);
+                    channel3[2].noteOn(a[i] + 3, 60);
+                    Thread.sleep(500);
+                    channel1[2].noteOff(a[i]);
+                    channel2[2].noteOff(a[i] + 1);
+                    channel3[2].noteOff(a[i] + 3);
+                    break;
+                }
+                default: {
+                    channel1[2].noteOn(a[i], 60);
+                    channel2[2].noteOn(a[i] + 2, 60);
+                    channel3[2].noteOn(a[i] + 4, 60);
+                    Thread.sleep(500);
+                    channel1[2].noteOff(a[i]);
+                    channel2[2].noteOff(a[i] + 2);
+                    channel3[2].noteOff(a[i] + 4);
+                }
             }
         }
     }
